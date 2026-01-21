40646cab53b2851fa7c3f8c42389c031029abbfd
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-01-21 19:13:04 +0800

    feat: add core barrel exports

---
+++ lib/services/services.dart
@@ -0,0 +1,13 @@
+/// Core Services Barrel Export
+///
+/// Import this file to access all core services:
+/// ```dart
+/// import 'package:get_gains_app/services/services.dart';
+/// ```
+library;
+
+export 'api/api_client.dart';
+export 'api/interceptors.dart';
+export 'database/app_database.dart';
+export 'storage/secure_storage_service.dart';
+export 'sync/sync_service.dart';
