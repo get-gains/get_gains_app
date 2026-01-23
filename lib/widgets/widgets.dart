f0cd9e423cda79ad7b555720d82ba29f62877b0e
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-01-23 19:24:41 +0800

    feat: create reusable button, card and text field widget

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,9 @@
+// lib/widgets/widgets.dart
+//
+// Barrel file for all app widgets.
+// Import this file to access all custom widgets:
+// import 'package:get_gains_app/widgets/widgets.dart';
+
+export 'app_button.dart';
+export 'app_card.dart';
+export 'app_text_field.dart';
