01b58aea5d06b4044bedf4a800b94942c9dac102
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-01-27 15:33:28 +0800

    feat: setup register providers

---
+++ lib/features/auth/presentation/presentation.dart
@@ -0,0 +1,9 @@
+/// Auth Presentation Layer
+///
+/// Export all auth presentation components.
+library;
+
+export 'providers/providers.dart';
+
+// Screens will be added later
+// export 'screens/screens.dart';
