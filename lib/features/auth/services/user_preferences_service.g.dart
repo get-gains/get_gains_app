db0da1e70b2780e722ee71f7eed95e82b3c5bd03
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-01-27 15:32:29 +0800

    feat: setup user service and google sign in

---
+++ lib/features/auth/services/user_preferences_service.g.dart
@@ -0,0 +1,62 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_preferences_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+/// Provider for UserPreferencesService
+
+@ProviderFor(userPreferencesService)
+const userPreferencesServiceProvider = UserPreferencesServiceProvider._();
+
+/// Provider for UserPreferencesService
+
+final class UserPreferencesServiceProvider
+    extends
+        $FunctionalProvider<
+          UserPreferencesService,
+          UserPreferencesService,
+          UserPreferencesService
+        >
+    with $Provider<UserPreferencesService> {
+  /// Provider for UserPreferencesService
+  const UserPreferencesServiceProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'userPreferencesServiceProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$userPreferencesServiceHash();
+
+  @$internal
+  @override
+  $ProviderElement<UserPreferencesService> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  UserPreferencesService create(Ref ref) {
+    return userPreferencesService(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(UserPreferencesService value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<UserPreferencesService>(value),
+    );
+  }
+}
+
+String _$userPreferencesServiceHash() =>
+    r'8959779a89160e9dc3dcb0ac2cc943aeda33357c';
