966ad183d15b5e79bfadcb92df07d346d5a44c6d
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-02-01 22:36:11 +0800

    feat: add subscription providers

---
+++ lib/features/subscription/presentation/providers/providers.dart
@@ -0,0 +1,7 @@
+// lib/features/subscription/presentation/providers/providers.dart
+
+/// Subscription providers exports
+library;
+
+export 'subscription_provider.dart';
+export 'subscription_guard.dart';
