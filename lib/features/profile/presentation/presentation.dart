63dfcc5ef8d1fa2162f8112d1568a3248e3a459e
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-02-15 19:45:38 +0800

    feat: add onboarding screen

--- lib/features/profile/presentation/presentation.dart
+++ lib/features/profile/presentation/presentation.dart
@@ -8,3 +8,4 @@ export 'providers/profile_provider.dart';
 export 'providers/user_profile_provider.dart';
 export 'screens/edit_profile_screen.dart';
 export 'screens/profile_screen.dart';
+export 'widgets/onboarding_sheet.dart';
