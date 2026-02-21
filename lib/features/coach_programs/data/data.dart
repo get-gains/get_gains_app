d0c5ed43617f9f7015a4464f026be0894f41140a
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-02-21 20:55:52 +0800

    feat: add coach program repository models

---
+++ lib/features/coach_programs/data/data.dart
@@ -0,0 +1,7 @@
+/// Coach Programs Data Layer
+///
+/// Contains models, repository, and data sources for coach programs feature.
+library;
+
+export 'models/models.dart';
+export 'coach_program_repository.dart';
