2ce84dbe2f6f0029c504cf495100af1873d081a6
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-03-12 16:14:30 +0800

    feat: add leaderboard screens

--- lib/providers/router_provider.dart
+++ lib/providers/router_provider.dart
@@ -20,6 +20,7 @@ import '../features/gains_coins/presentation/screens/coin_reward_screen.dart';
 import '../features/gains_coins/presentation/screens/shop_screen.dart';
 import '../features/gains_coins/presentation/screens/cosmetic_detail_screen.dart';
 import '../features/gains_coins/presentation/screens/inventory_screen.dart';
+import '../features/gains_coins/presentation/screens/leaderboard_screen.dart';
 import '../features/gains_coins/data/models/cosmetic_model.dart';
 import 'auth_state_provider.dart';
 import '../features/programs/screens/program_screen.dart';
@@ -126,6 +127,7 @@ class AppRoutes {
   static const String shop = '/shop';
   static const String cosmeticDetail = '/shop/cosmetic';
   static const String inventory = '/inventory';
+  static const String leaderboard = '/leaderboard';
 }
 
 /// Router Provider
@@ -649,6 +651,10 @@ GoRouter router(Ref ref) {
       GoRoute(
         path: AppRoutes.inventory,
         builder: (context, state) => const InventoryScreen(),
+      ),
+      GoRoute(
+        path: AppRoutes.leaderboard,
+        builder: (context, state) => const LeaderboardScreen(),
       ),
     ],
 
