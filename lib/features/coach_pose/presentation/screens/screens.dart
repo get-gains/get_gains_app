8ee2afe1c7141a2d5e6e55f95a4feacc8a7cc4ba
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-03-15 05:51:23 +0800

    feat: add Coach View Form Screen and routing for form preview

--- lib/features/coach_pose/presentation/screens/screens.dart
+++ lib/features/coach_pose/presentation/screens/screens.dart
@@ -4,4 +4,5 @@ library;
 export 'exercise_list_screen.dart';
 export 'create_exercise_screen.dart';
 export 'exercise_detail_screen.dart';
+export 'coach_view_form_screen.dart';
 export 'form_recording_screen.dart';
