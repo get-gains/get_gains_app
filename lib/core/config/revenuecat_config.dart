65d4fe25ea41c9cab1d16c35a8601e8a52d3f0f2
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-04-19 23:04:09 +0800

    refactor: update RevenueCatConfig to use dotenv for API keys

--- lib/core/config/revenuecat_config.dart
+++ lib/core/config/revenuecat_config.dart
@@ -1,13 +1,12 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class RevenueCatConfig {
   RevenueCatConfig._();
 
-  static const String androidApiKey = String.fromEnvironment(
-    'REVENUECAT_ANDROID_API_KEY',
-  );
+  static String get androidApiKey =>
+      dotenv.env['REVENUECAT_ANDROID_API_KEY'] ?? '';
 
-  static const String iosApiKey = String.fromEnvironment(
-    'REVENUECAT_IOS_API_KEY',
-  );
+  static String get iosApiKey => dotenv.env['REVENUECAT_IOS_API_KEY'] ?? '';
 
   static const String entitlementId = 'premium';
 }
